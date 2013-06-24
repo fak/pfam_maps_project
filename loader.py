21aa312aeebbee9aab53c5972f8d55379ed93315
Felix <momo.sander@googlemail.com>
2013-06-24 18:47:41 +0100

    added loader script

--- loader.py
+++ loader.py
@@ -1,11 +1,12 @@
-"""
-Function:  master
+"""Script:  loader.py
 
-Goes through all necessary steps.
-    --------------------
-    Author:
-    Felix Kruger
-    fkrueger@ebi.ac.uk
+Load the mapping of Pfam-A domains. The main function is loader, defined at the bottom of the document. Specify release and version of the mapping on command line. eg.: $> python loader.py chembl_15 0_1
+
+--------------------
+Author:
+Felix Kruger
+fkrueger@ebi.ac.uk
+
 """
 import os
 import sys
@@ -13,30 +14,42 @@ import queryDevice
 import yaml
 
 
+def readfile(path, key_name, val_name):
+    '''Read two columns from a tab-separated file into a dictionary.
 
-def readfile(path, keykey, valkey):
-    '''Load the list of domains.'''
+    Inputs:
+    path -- filepath
+    key_name -- name of the column holding the key
+    val_name -- name of the column holding the value
+
+    '''
     infile = open(path, 'r')
     lines = infile.readlines()
     infile.close()
     lkp = {}
     els = lines[0].rstrip().split('\t')
     for i, el in enumerate(els):
-        if el == keykey:
-            idx = i
-        if el == valkey:
-            vdx = i
+        if el == key_name:
+            key_idx = i
+        if el == val_name:
+            val_idx = i
     for line in lines[1:]:
         elements = line.rstrip().split('\t')
-        lkp[elements[idx]] = elements[vdx]
+        lkp[elements[key_idx]] = elements[val_idx]
     return  lkp
 
 
 
 def retrieve_acts(dom_string, params):
-    """Run a query that joins act_id, tid, component_id, domain_id and domain_name."""
-    acts = queryDevice.queryDevice(
-         """SELECT DISTINCT act.activity_id, ass.tid, tc.component_id, cd.compd_id, dm.domain_name
+    """Run a query for act_id, tid, component_id, compd_id and domain_name.
+
+    Inputs:
+    dom_string -- A string specifying the domain names eg. "7tm_1','Pkinase','Pkinase_tyr"
+    params -- dictionary holding details of the connection string
+
+    """
+    acts = queryDevice.queryDevice("""
+    SELECT DISTINCT act.activity_id, ass.tid, tc.component_id, cd.compd_id, dm.domain_name
                       FROM activities act
                       JOIN assays ass
                           ON ass.assay_id = act.assay_id
@@ -57,14 +70,19 @@ def retrieve_acts(dom_string, params):
                        'log Ki', 'log Kd', 'log IC50', 'Log EC50', 'Log AC50'
                        'pKi', 'pKd', 'pIC50', 'pEC50', 'pAC50'
                         )
-                     AND dm.domain_name IN('%(dom_string)s')"""
-                           %locals() ,params['release'], params['user'], params['pword'], params['host'], params['port'])
+                     AND dm.domain_name IN('%(dom_string)s')
+                     """ %locals() ,params['release'], params['user'], params['pword'], params['host'], params['port'])
     return acts
 
 
 
 def map_ints(acts):
-    """ Map interactions to activity ids."""
+    """ Map interactions to activity ids.
+
+    Inputs:
+    acts -- output of the sql query in retrieve_acts().
+
+    """
     lkp = {}
     for act in acts:
         (act_id, tid, component_id, compd_id, domain_name) = act
@@ -78,7 +96,12 @@ def map_ints(acts):
 
 
 def flag_conflicts(lkp):
-    """Flag conflicts."""
+    """Flag conflicts.
+
+    Input:
+    lkp -- a lookup dictionary of the form lkp[act_id][compd_id] = domain_name
+
+    """
     flag_lkp  = {}
     for act_id in lkp.keys():
         if len(lkp[act_id].keys()) == 1:
@@ -92,8 +115,15 @@ def flag_conflicts(lkp):
 
 
 def apply_manual(lkp, flag_lkp, infile):
-    """Apply manually resolved conflicts."""
-    infile = open(infile, 'r')
+    """Apply manually resolved conflicts.
+
+    Input:
+    lkp -- a dictionary of the form lkp[act_id][compd_id] = domain_name
+    flag_lkp -- a dictionary of the form flag_lkp[act_id] = (conflict_flag, manual_flag)
+    path -- a filepath to the resolved_conflicts table
+
+    """
+    infile = open(path, 'r')
     lines = infile.readlines()
     infile.close()
     for line in lines[1:]:
@@ -106,9 +136,16 @@ def apply_manual(lkp, flag_lkp, infile):
     return(lkp, flag_lkp)
 
 
-def write_table(lkp, flag_lkp, outfile):
-    """ write a table containing activity_id, domain_id, tid, conflict_flag, type_flag."""
-    out = open(outfile, 'w')
+def write_table(lkp, flag_lkp, path):
+    """ Write a table containing activity_id, domain_id, tid, conflict_flag, type_flag.
+
+    Input:
+    lkp -- a dictionary of the form lkp[act_id][compd_id] = domain_name
+    flag_lkp -- a dictionary of the form flag_lkp[act_id] = (conflict_flag, manual_flag)
+    path -- a filepath to the output file
+
+    """
+    out = open(path, 'w')
     out.write("map_id\tactivity_id\tcompd_id\tdomain_name\tconflict_flag\tmanual_flag\n")
     counter = 0
     for act_id in lkp.keys():
@@ -120,8 +157,15 @@ def write_table(lkp, flag_lkp, outfile):
             out.write("%(counter)i\t%(act_id)i\t%(compd_id)i\t%(domain_name)s\t%(conflict_flag)i\t%(manual_flag)i\n"%locals())
     out.close()
 
+
+
 def upload_sql(params):
-    """ Load SQL table using connection string defined in global parameters."""
+    """ Load SQL table using connection string defined in global parameters.
+
+    Input:
+    params -- dictionary holding details of the connection string.
+
+    """
     status = os.system("cp data/pfam_maps_v_%(version)s.tab data/pfam_maps.txt" % params)
     if status != 0:
         sys.exit("Error copying data/pfam_maps_v_%(version)s.tab to data/pfam_maps.txt" % params)
@@ -135,20 +179,20 @@ def upload_sql(params):
 
 
 def loader(release, version):
+    """Main function to load the mapping of Pfam-A domains.
 
+    Inputs from command line:
+    release -- chembl release eg "chembl_12"
+    version -- version of the mapping eg 0_1
+
+"""
     # Read config file.
     paramFile = open('mpf.yaml')
     #paramFile = open('mpf_local.yaml')
+    #paramFile = open('mpf_example.yaml')
     params = yaml.safe_load(paramFile)
-    user = params['user']
-    pword = params['pword']
-    host = params['host']
-    port = params['port']
-    th = params['threshold']
-    params['release'] = release
-    params['version'] = version
 
-    # Load the list of domains.
+    # Load the list of validated domains.
     domains = readfile('data/valid_pfam_v_%(version)s.tab' % locals(), 'pfam_a', 'pfam_a')
     dom_string = "','".join(domains.keys())
 
@@ -169,17 +213,13 @@ def loader(release, version):
     outfile = 'data/pfam_maps_v_%(version)s.tab' %locals()
     write_table(lkp, flag_lkp, outfile)
 
-    # Load SQL table using connection string defined in global parameters.
+    # Load SQL table.
     upload_sql(params)
 
 if __name__ == '__main__':
     import sys
-
     if len(sys.argv) != 3:  # the program name and the two arguments
-
         sys.exit("Must specify release (eg. chembl_15) and version (eg. 0_1).")
-
-
     release = sys.argv[1]
     version = sys.argv[2]
 
