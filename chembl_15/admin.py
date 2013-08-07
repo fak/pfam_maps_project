855abbfd529d8e80db4fe75e0a6195bb60113301
Felix <momo.sander@googlemail.com>
2013-08-07 11:05:16 +0100

    adapted admin.py to schema changes

--- chembl_15/admin.py
+++ chembl_15/admin.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 
 class PfamMapsAdmin(admin.ModelAdmin):
         #fields = ['activity_id', 'conflict_flag', 'domain_name']
-        list_display = ('activity_id', 'domain_name', 'conflict_flag', 'manual_flag', 'compd_id')
+        list_display = ('activity_id', 'domain_name', 'category_flag', 'status_flag', 'manual_flag', 'compd_id')
 admin.site.register(PfamMaps, PfamMapsAdmin)
