755a144709ec84a7a98d01b0194bf3f21b16196f
Felix <momo.sander@googlemail.com>
2013-08-08 15:19:57 +0100

    cleaning repo

--- chembl_15/urls.py
+++ chembl_15/urls.py
@@ -10,8 +10,12 @@ urlpatterns = patterns('chembl_15.views',
     url(r'^evidence/$', 'evidence_portal', name='evidence_portal'),
     url(r'^evidence/(?P<pfam_name>[\w-]+)/$', 'evidence', name='evidence'),
     url(r'^conflicts/$', 'conflicts_portal', name='conflicts_portal'),
+    url(r'^resolved/$', 'resolved_portal', name='resolved_portal'),
     url(r'^conflicts/(?P<conflict_id>.+)/$', 'conflicts', name = 'conflicts'),
-    url(r'^vote/conflicts/(?P<act>\d+)/(?P<conflict_id>.+)/$', 'vote' , name = 'vote'),
+    url(r'^resolved/(?P<conflict_id>.+)/$', 'resolved', name = 'resolved'),
+    url(r'^vote/conflicts/(?P<act>\d+)/(?P<conflict_id>.+)/$', 'vote_on_activity' , name = 'vote_on_activity'),
+    url(r'^vote/conflicts/(?P<assay_id>CHEMBL\d+)/(?P<conflict_id>.+)/$', 'vote_on_assay' , name =  'vote_on_assay'),
+    url(r'^revoke/resolved/(?P<assay_id>CHEMBL\d+)/(?P<conflict_id>.+)/$', 'revoke_assay' , name  =  'revoke_assay'),
     url(r'^details/conflicts/(?P<act>\d+)/$', 'details', name = 'details'),
     )
 
